4ff888f918464aa025951219b3242a1524d0b54c
Oliver <ot17409@bristol.ac.uk>
2018-04-17 14:32:25 +0100

    Fixed code for gateset synthesis, working on implementing the 4 qubit gate decomp

--- matrixmul.f90
+++ matrixmul.f90
@@ -4,7 +4,7 @@ implicit none
 
 integer, parameter :: dp=selected_real_kind(15,300)
 integer :: n, qubits, numofdecomp, i, j, counter
-integer, allocatable, dimension(:) :: p
+integer, allocatable, dimension(:) :: p, gatenum
 
 real(kind=dp), parameter :: invr2=1/sqrt(real(2,kind=dp)), invr3=1/sqrt(real(3,kind=dp)), invr6=1/sqrt(real(6,kind=dp))
 real(kind=dp), parameter :: r2=sqrt(real(2,kind=dp)) 
@@ -24,12 +24,14 @@ allocate(u(n,n,n*n))
 allocate(uprod(n,n))
 allocate(p(n))
 allocate(gateseq(n,n,n*n))
+allocate(gatenum(numofdecomp))
 
 
 ident=0.0_dp
 unitary=0.0_dp
 u=0.0_dp
 gateseq=0.0_dp
+gatenum=1
 
 !#make identity
 ident=identity(n)
@@ -40,10 +42,7 @@ do i=1, size(u,3)
 end do
 gateseq=u
 !#init uprod as ident
-uprod=identity(n)
-
-
-!#write(*,*) int(ident)
+!uprod=identity(n)
 
 !#make unitary
 if (qubits==2) then 
@@ -90,15 +89,9 @@ write(*,*) unitary
 !#!!! make unitary gates
 
 !#print*, p(n), p(n-1), p(1)
-!#write(*,*) uprod
-!#write(*,*) u(:,:,1)
 uprod=unitary
 do i=1,n !#col
   do j=1,n-1 !#row
-    !#write(*,*)
-    !#write(*,*) uprod
-	!#print*, 
-	!#print*, p(n+1-j
     if(p(n-j+1).ne.p(i)) then
       call makeunitary(p(n-j),p(n-j+1),p(i), uprod, u(:,:,(i-1)*n+j))
     end if
@@ -111,33 +104,31 @@ print*, 'unitaries'
 uprod=unitary
 do i=1, n*n
   if (icheck(u(:,:,i))==0) then 
-    print*, 'u non identity',i
-    write(*,*) u(:,:,i)
-    print*,
+    !print*, 'u non identity',i
+    !write(*,*) u(:,:,i)
+    !print*,
     uprod=matmul(uprod(:,:),u(:,:,i))
-    write(*,*) uprod
-    print*,
+    !write(*,*) uprod
+    !print*,
   end if
 end do
 
-!uprod=ident
-
-
-print*,
-print*, unitarycheck(u,unitary)
+!print*, unitarycheck(u,unitary)
+!print*, icheck(uprod)
 !if (unitarycheck(u,unitary)==1) then
+!  if (icheck(uprod)==1) then
   print*, 'THIS IS UNITARY'
   call invert(u,gateseq,counter)
   call gateset(gateseq)
 !end if
 
-do i=1,n*n
-if (icheck(gateseq(:,:,i))==0) then
+!do i=1,n*n
+!if (icheck(gateseq(:,:,i))==0) then
 
 !write(*,*) gateseq(:,:,i)
-print*,
-end if
-end do
+!print*,
+!end if
+!end do
 
 do i=1, counter
   uprod=matmul(uprod,gateseq(:,:,i))
@@ -148,7 +139,7 @@ print*, 'Unitary matrix from', counter-1, 'gates'
 print*, '------------------------------------------------------------------'
 
 print*,
-write(*,*) uprod
+!write(*,*) uprod
 !write(*,*) icheck(uprod)
 !#write(*,*) matmul(gateseq(:,:,1),gateseq(:,:,2))
 
@@ -159,6 +150,7 @@ deallocate(u)
 deallocate(uprod)   
 deallocate(p)
 deallocate(gateseq)
+deallocate(gatenum)
 
 !#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 contains
@@ -171,8 +163,8 @@ subroutine gateset(matrix)
   do i=1, n
     if (icheck(matrix(:,:,i))==0) then
       print*, 'gate', i
-      write(*,*) matrix(:,:,i)
-      print*,
+      !write(*,*) matrix(:,:,i)
+      !print*,
     end if
   end do
 end subroutine gateset
@@ -194,6 +186,34 @@ subroutine invert(matrix,inverted,count)
 end subroutine invert
 
 !#!!!! Check product gives identity 
+function icheck(uni)
+  real(kind=dp) :: icheck
+  real(kind=dp), dimension(:,:), intent(in) :: uni
+  integer :: i, j
+
+icheck=1
+!#check ident
+itest:do i=1, size(uni,1)
+  do j=1, size(uni,1)
+    if (i.ne.j) then
+      if (abs(uni(i,j))>=1e-10) then
+!        print*, 'not ident off diag'
+        icheck=0
+        exit itest
+      end if
+    else if (i.eq.j) then
+      if (abs(abs(uni(i,j))-1)>=1e-10) then
+!        print*, 'not identity diag', uni(i,j)
+!        write(*,*) uni
+        icheck=0
+        exit itest
+      end if
+    end if
+  end do
+end do itest
+end function icheck
+
+!#!!!! Check product gives identity 
 function unitarycheck(umatrices, uni)
   real(kind=dp) :: unitarycheck
   real(kind=dp), dimension(:,:,:), intent(in) :: umatrices
@@ -203,14 +223,10 @@ function unitarycheck(umatrices, uni)
 
 unitarycheck=1
 uprod=uni
-!#write(*,*) uprod
-!#print*,
 !#do u_n*u_n-1*...*u1*Unitary=Ident
 do i=1, size(umatrices,3)-1
   if (icheck(umatrices(:,:,n*n-i))==0) then
     uprod=matmul(uprod(:,:), umatrices(:,:,i))
-   !# write(*,*) uprod
-   !# write(*,*)
   end if
 end do
 
@@ -218,12 +234,32 @@ unitarycheck=icheck(uprod)
 
 end function unitarycheck 
 
+!#!!! find type of u
+subroutine makeunitary(row1,row2,col,ucurrent, ugate)
+  real(kind=dp) :: c,s,r
+  real(kind=dp), dimension(:,:), intent(inout) :: ucurrent, ugate
+  integer, intent(in) :: row1, row2, col
+  
+  ugate=identity(size(ucurrent,1))
+  c=0.0
+  s=0.0
+  r=0.0
+
+call givensrot(ucurrent(col,row1), ucurrent(col,row2), c,s,r)
+  ugate(row1,row1)=c
+  ugate(row1,row2)=-s
+  ugate(row2,row1)=s
+  ugate(row2,row2)=c
+! if (icheck(ugate)==0) then 
+!end if
+ucurrent=matmul(ucurrent,ugate)
+end subroutine makeunitary
+
 !#!!! Calc givens rotation
 subroutine givensrot(a, b, c, s, r)
   real(kind=dp) :: a, b, c, s, r, h, d
 h=0.0
 d=0.0
-!#print*,
  !#write(*,*) 'a',a,'b',b,'c',c,'s',s
 if (abs(b)>=1e-1) then
   h=hypot(a,b)
@@ -239,32 +275,6 @@ else
 end if
 end subroutine givensrot  
 
-!#!!! find type of u
-subroutine makeunitary(row1,row2,col,ucurrent, ugate)
-  real(kind=dp) :: c,s,r
-  real(kind=dp), dimension(:,:), intent(inout) :: ucurrent, ugate
-  integer, intent(in) :: row1, row2, col
-  
-  ugate=identity(size(ucurrent,1))
-  c=0.0
-  s=0.0
-  r=0.0
-
-!#print*, 'r1',row1,'r2',row2,'col',col
-call givensrot(ucurrent(col,row1), ucurrent(col,row2), c,s,r)
-!#print*, 'c=', c, 's=', s
-  ugate(row1,row1)=c
-  ugate(row1,row2)=-s
-  ugate(row2,row1)=s
-  ugate(row2,row2)=c
- if (icheck(ugate)==0) then 
-  !#print*, 'ugate'
-  !#write(*,*) ugate
-  !#print*,
-end if
-ucurrent=matmul(ucurrent,ugate)
-
-end subroutine makeunitary
 
 !#!!!!! make identiy matrix dim n
 function identity(n)
@@ -276,37 +286,6 @@ do i=1, n
   identity(i,i) =1.0_dp
 end do
 end function identity
-
-
-!#!!!! Check product gives identity 
-function icheck(uni)
-  real(kind=dp) :: icheck
-  real(kind=dp), dimension(:,:), intent(in) :: uni
-  integer :: i, j
-
-icheck=1
-
-!#check ident
-itest:do i=1, size(uni,1)
-  do j=1, size(uni,1)
-    if (i.ne.j) then
-      if (abs(uni(i,j))>=1e-10) then
-        print*, 'not ident off diag'
-        icheck=0
-        exit itest
-      end if
-    else if (i.eq.j) then
-      if ((abs(uni(i,j))-1)>=1e-10) then
-        print*, 'not identity diag'
-        icheck=0
-        exit itest
-      end if
-    end if
-  end do
-end do itest
-
-end function icheck
-
 
 end program matrixmul
 
