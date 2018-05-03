1448db2a9c02be1834aeb8605d6a2e133f1cf2ef
Oliver <oliver.thomas404@gmail.com>
2018-05-03 18:50:59 +0100

    added in figures and some example code

--- src/batman.py
+++ src/batman.py
@@ -12,16 +12,17 @@ def shoulders(x):
     return 4.2 - 0.5*x - 2.8*semicirc(0.5*x-0.5)
 
 def bot(x):
-    return semicircle(abs(2-x)-1) - x**2/11 + 0.5*x -3
+    return semicirc(abs(2-x)-1) - x**2/11 + 0.5*x -3
 
-
+def xl(min,max):
+    return np.linspace(min,max)
 #sf(x) = sqrt(1-x^2)                           # semicircle
 #ef(x) = 3*sf(x/7)                             # ellipse
 #sh(x) = 4.2 - .5*x -2.8*sf(.5*x -.5)          # shoulders
 #bf(x) = sf(abs(2 - x) - 1) - x^2/11 + .5*x -3 # bottom
 
 cl    = [(0,1.7), (.5,1.7), (.8,2.6), (1,.9)] # cowl right
-cl2   = [(-x,y) for (x,y) in cl]              # cowl left
+cl2   = [(0,1.7), (-.5,1.7), (-.8,2.6), (-1,.9)]               # cowl left
 
 def p(f,xmin,xmax):
 #    "symmetric plot across y-axis"
@@ -30,5 +31,11 @@ def p(f,xmin,xmax):
     return p1 + p2
 
 #p(ellipse,3,7) + p(-ellipse,4,7) + p(shoulders,1,3) + p(bot,0,4) + line(cl) +line(cl2)
+x = np.linspace(-10,10)
 
-plot
+y=ellipse(xl(3,7)) -ellipse(xl(4,7)) + shoulders(xl(1,3)) + bot(xl(0,4)) 
+
+plt.plot(x, y) 
+
+plt.show()
+
