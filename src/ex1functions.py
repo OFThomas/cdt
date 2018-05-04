76f91da596136d0933040f833d2be9975041b6c3
Oliver <oliver.thomas404@gmail.com>
2018-05-04 14:16:32 +0100

    added data for ex3data.py plot

--- src/ex1functions.py
+++ src/ex1functions.py
@@ -7,5 +7,5 @@ t = np.arange(0., 5., 0.2)
 # red dashes, blue squares and green triangles
 plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
 
-plt.savefig('ex1.png', format='png')
-#plt.show()
+#plt.savefig('ex1.png', format='png')
+plt.show()
