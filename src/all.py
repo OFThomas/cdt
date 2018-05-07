3faff71977254da9cc7f2592c44ffa9f13b213e7
Oliver <oliver.thomas404@gmail.com>
2018-05-07 15:24:15 +0100

    added axis labels into example

--- src/all.py
+++ src/all.py
@@ -14,8 +14,8 @@ def ex1():
     plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
     
     #labels
-    plt.ylabel('Y data /units')
-    plt.xlabel('X data /units')
+   # plt.ylabel('Y data /units')
+   # plt.xlabel('X data /units')
 
     plt.title('3 functions on one graph!')
     #plt.savefig('ex1.png', format='png')
