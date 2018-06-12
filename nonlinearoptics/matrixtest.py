440d9396210b5f013d0db33c7f60ea690b9f3516
Oliver <ot17409@bristol.ac.uk>
2018-06-12 18:02:22 +0100

    started symbolic matrix gen of squeezing, beamsplit... but can't get block matrix form working

--- nonlinearoptics/matrixtest.py
+++ nonlinearoptics/matrixtest.py
@@ -1,12 +1,62 @@
 from sympy import *
 init_printing(use_unicode=True)
 
-a = symbols('a')
+c1=MatrixSymbol('c1',2,2)
+s1=MatrixSymbol('s1',2,2)
 
-x=Matrix([[0,1],[1,0]])
+#cbig=MatrixSymbol('c', 2,2)
+#sbig=MatrixSymbol('s', 2,2)
+c,s = symbols('c,s')
+cbig, sbig = symbols('c s')
+
+
+a,b,c,d = symbols('a,b,c,d')
+e,f,g,h = symbols('e,f,g,h')
+
+#Squeezing mode size
+sqsize=2
+
+#total dim
+n=2
+
+#make symbolic matrix
+smsq=Matrix(n,n, lambda i,j: cbig if i==j else sbig if i+j==n-1 else 0)
+print('smsq')
+pprint(smsq)
+
+#single mode squeezer
+c=Matrix(sqsize,sqsize, lambda i,j: a )
+s=Matrix(sqsize,sqsize, lambda i,j: b)
+
+testsmsq=Matrix(n,n, lambda i,j: c if i==j else s if i+j==n-1 else 0)
+print('testsmsq')
+pprint(testsmsq)
+
+print('subs in matrices')
+test=smsq.subs({cbig:c, sbig:s})
+pprint(test)
+smsq=test
+print('Print smsq')
+pprint(smsq)
+
+#print('eigenvlues, multiplicities')
+#print(smsq.eigenvals())
+#print('eigenvectors')
+#pprint(x.eigenvects())
+
+p, d = smsq.diagonalize()
+pprint(p)
+pprint(d)
+
+pprint(p*d*p**-1)
+
+
+"""
+Eq(a,1)
+
 pprint (x)
 
-print x.eigenvals()
+print(x.eigenvals())
 pprint(x.eigenvects())
 
 p, d = x.diagonalize()
@@ -15,7 +65,6 @@ pprint(d)
 
 pprint(p*d*p**-1)
 
-
-
+"""
 
 
