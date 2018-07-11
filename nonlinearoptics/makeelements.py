a2efe98e9d68ad4b15cd973a11cc7196e5028961
Oliver <ot17409@bristol.ac.uk>
2018-07-11 10:54:14 +0100

    moved into a class, still working

---
+++ nonlinearoptics/makeelements.py
@@ -0,0 +1,192 @@
+from sympy import *
+from sympy.physics.quantum.dagger import Dagger
+from sympy.physics.quantum import TensorProduct 
+from mpmath import mp
+from sympy.abc import alpha, beta, xi, zeta
+
+
+class Makeelements():
+    
+    def __init__(self,nspace,nspectral,sq1_mode1,sq1_mode2, sq2_mode1, sq2_mode2, phasemode, phaseangle, bsmode1,bsmode2, bsangle):
+   
+        self.nspace=nspace
+        self.n=nspace*nspectral
+        self.nspectral=nspectral
+        
+        self.a=[None]*self.n
+        for i in range(0,self.nspace):
+            for j in range(0,self.nspectral):
+                self.a[i*(self.nspectral)+j]=symbols('a%d%d' % (i, j))
+        
+        # phase shift
+        omega=[None]*(nspectral)
+        for i in range(0,nspectral):
+            omega[i]=symbols('omega:%d' % (i))
+
+        omega[0]=pi/2
+
+        ########### Define squeezing symbols
+        xi=[None]*(self.n**2)
+        for i in range(0,self.n**2):
+            xi[i]=symbols('xi%d' % (i))
+
+                    
+        #modes 0 & 1
+        xi[1]=1
+        xi[2]=2
+
+        #modes 2 & 3
+        xi[5]=1
+        xi[6]=2
+
+
+        #make active s block anti-diag
+        xi[0]=0
+        xi[3]=0
+        xi[4]=0
+        xi[7]=0
+
+
+        self.modes=self.makemodes()
+      
+        #block form for symplectic matrix
+        self.al = MatrixSymbol('alpha', self.n,self.n)
+        self.be = MatrixSymbol('beta',self.n,self.n)
+
+        #make block form for symplectic matrix
+        self.block=BlockMatrix([[self.al,self.be],
+                        [conjugate(self.be), conjugate(self.al)]])
+ 
+        #make tmsq on modes 0,1 squeezing xi
+        self.sq1=self.makesq(sq1_mode1,sq1_mode2, xi)
+        pprint(self.sq1)
+
+        #make tmsq on modes 2,3
+        self.sq2=self.makesq(sq2_mode1,sq2_mode2, xi)
+        pprint(self.sq2)
+
+        #make phase shifter
+        self.ps=self.makeps(phasemode,omega,phaseangle)
+        pprint(self.ps)
+
+        #make beamsplitter 
+        self.bs=self.makebs(bsmode1,bsmode2,bsangle)
+        pprint(self.bs)
+
+        return
+
+    def makemodes(self):
+        #Make optical modes
+        ablk=MatrixSymbol('a',self.n,1)
+        #build block matrix for modes
+        bmodes=BlockMatrix([[ablk],[conjugate(ablk)]])
+        #make a size n vector for a
+        modematrix=Matrix(self.n,1, lambda i,j: self.a[i] )
+        #substitution for a modes
+        modes=self.matsubs(bmodes,ablk,modematrix,0,0,0,0)
+        return modes
+
+    def makeps(self,mode1, omega,phaseangle):
+        ##Phase shifter
+        m1=mode1*self.nspectral
+        phasespace=Matrix(self.n,self.n, lambda i,j: exp(sqrt(-1)*omega[i%self.nspectral])
+                if ((i==j)and((m1<=i)and(i<m1+self.nspectral))) else 1 if(i==j) else 0)
+        # symplectic phase shifer
+        mps=Matrix(self.matsubs(self.block,self.al,phasespace,self.be,zeros(self.n),0,0))
+        return mps
+
+    def makebs(self,mode1,mode2,theta):
+        #Make beamsplitter 
+        beamspace= Matrix(self.nspace,self.nspace, lambda i,j: cos(theta) 
+                if ((i==j)and((i==mode1)or(i==mode2))) 
+                else  sqrt(-1)*sin(theta) if (i+j)==(mode1+mode2)and((i==mode1)or(i==mode2)) 
+                else 1 if (i==j)and((i!=mode1)or(i!=mode2)) else 0)
+        beamsplitter=TensorProduct(beamspace,eye(self.nspectral))    
+        # symplectic beamsplitter
+        mbs=Matrix(self.matsubs(self.block,self.al,beamsplitter,self.be,zeros(self.n),0,0))
+        return mbs
+
+    def makesinglesq(self):
+        #Single mode squeezer
+        c1=Matrix(self.n,self.n, lambda i,j: c(xi[i%self.nspectral]) if i==j else 0)
+        s1=Matrix(self.n,self.n, lambda i,j: s(xi[i%self.nspectral]) if i==j else 0)
+        return c1, s1
+        
+    def makesq(self,mode1,mode2, sqparam):
+        #Two mode squeezer
+        #for off-diagonal
+        n=self.n
+        nspec=self.nspectral
+        #for diagonal
+        m1s=mode1*nspec
+        m2s=mode2*nspec
+        #diagonal part cosh on modes, I elsewhere
+        c2=Matrix(n,n, lambda i,j: cosh(sqparam[i+m2s]) 
+                if (i==j)and((m1s<=i<m1s+nspec)or(m2s<=i<m2s+nspec))
+                else 1 if i==j else 0 )
+        #off diag sinh on modes, zero else
+        s2=Matrix(n,n, lambda i,j: sinh(sqparam[2*i+j-m1s]) 
+                if ((m1s<=i<m1s+nspec)or(m2s<=i<m2s+nspec))and
+                ((m1s<=j<m1s+nspec)or(m2s<=j<m2s+nspec))
+                else 0)
+        # symplectic sqs
+        ms01=Matrix(self.matsubs(self.block,self.al,c2,self.be,s2,0,0))
+        return ms01
+
+    def matsubs(self,mat,aold,anew,bold,bnew,cold,cnew):
+        temp1=mat.subs(aold,anew)
+        temp2=temp1.subs(bold,bnew)
+        temp3=temp2.subs(cold,cnew)
+        return temp3
+
+    def justdoitplease(self,transform, modes):
+        modetransform=transform*modes
+        pprint(relational.Eq(Matrix(modes[0:self.n]),Matrix(modetransform[0:self.n])))
+        return 
+
+class Matrixops():
+
+    def __init__(self):
+        return
+    
+    def makematrixexact(self,matrix):
+        imax,jmax=matrix.shape
+        newmat=matrix[:,:] 
+        for j in range(0,jmax):
+            for i in range(0,imax):
+                newmat[i,j]=nsimplify(matrix[i,j], full=True)
+
+        return matrix
+
+    def takagi_for_unitary(self,A):
+        ### takes a unitary matrix A such that A^T = A, ###
+        ###    returns unitary U such that A = U U^T    ###
+        N = A.shape[0]
+        U = np.eye(N)
+
+        while A.shape[0] > 0:   
+            n = A.shape[0]
+            # constructing a vector v such that A*ybar = y
+            x = np.random.rand(n)+1j*np.random.rand(n)
+            x /= np.sqrt(np.dot(x.conj(),x))
+            Axbar = np.tensordot(A,x.conj(),(1,0))
+            mu = np.dot(x.conj(),Axbar)
+
+            if np.abs(mu)>1-1e-8:
+                y = mu**.5*x
+            else:
+                y = Axbar + x; y /= np.sqrt(np.dot(y.conj(),y))
+
+            # constructing a unitary V with first column = y
+            V = np.random.random((n,n))+1j*np.random.random((n,n))
+            V[:,0] = y
+            V,__ = np.linalg.qr(V)
+            # update A
+            A = np.tensordot(np.tensordot(V.conj().T,A,(1,0)),V.conj(),(1,0))[1:,1:]
+            # update U
+            Vi = np.eye(N,dtype=complex)
+            Vi[N-n:,N-n:] = V
+            U = np.tensordot(U,Vi,(1,0))
+
+        return U
+
--- nonlinearoptics/matrixtest.py
+++ nonlinearoptics/matrixtest.py
@@ -3,127 +3,10 @@ from sympy.physics.quantum.dagger import Dagger
 from sympy.physics.quantum import TensorProduct 
 from mpmath import mp
 from sympy.abc import alpha, beta, xi, zeta
+
+from makeelements import *
+
 init_printing(use_unicode=True)
-
-def makemodes():
-    #Make optical modes
-    ablk=MatrixSymbol('a',n,1)
-    #build block matrix for modes
-    bmodes=BlockMatrix([[ablk],[conjugate(ablk)]])
-    #make a size n vector for a
-    modematrix=Matrix(n,1, lambda i,j: a[i] )
-    #substitution for a modes
-    modes=matsubs(bmodes,ablk,modematrix,0,0,0,0)
-    return modes
-
-def makeps(mode1,phaseangle):
-    ##Phase shifter
-    m1=mode1*nspectral
-    phasespace=Matrix(n,n, lambda i,j: exp(sqrt(-1)*omega[i%nspectral])
-            if ((i==j)and((m1<=i)and(i<m1+nspectral))) else 1 if(i==j) else 0)
-    # symplectic phase shifer
-    mps=Matrix(matsubs(block,al,phasespace,be,zeros(n),0,0))
-    return mps
-
-def makebs(mode1,mode2,theta):
-    #Make beamsplitter 
-    beamspace= Matrix(nspace,nspace, lambda i,j: cos(theta) 
-            if ((i==j)and((i==mode1)or(i==mode2))) 
-            else  sqrt(-1)*sin(theta) if (i+j)==(mode1+mode2)and((i==mode1)or(i==mode2)) 
-            else 1 if (i==j)and((i!=mode1)or(i!=mode2)) else 0)
-    beamsplitter=TensorProduct(beamspace,eye(nspectral))    
-    # symplectic beamsplitter
-    mbs=Matrix(matsubs(block,al,beamsplitter,be,zeros(n),0,0))
-    return mbs
-
-def makesinglesq(n, nspec):
-    #Single mode squeezer
-    c1=Matrix(n,n, lambda i,j: c(xi[i%nspec]) if i==j else 0)
-    s1=Matrix(n,n, lambda i,j: s(xi[i%nspec]) if i==j else 0)
-    return c1, s1
-    
-def makesq(mode1,mode2, sqparam):
-    #Two mode squeezer
-    #for off-diagonal
-    nspec=nspectral
-    #for diagonal
-    m1s=mode1*nspec
-    m2s=mode2*nspec
-    #diagonal part cosh on modes, I elsewhere
-    c2=Matrix(n,n, lambda i,j: cosh(sqparam[i+m2s]) 
-            if (i==j)and((m1s<=i<m1s+nspec)or(m2s<=i<m2s+nspec))
-            else 1 if i==j else 0 )
-    #off diag sinh on modes, zero else
-    s2=Matrix(n,n, lambda i,j: sinh(sqparam[2*i+j-m1s]) 
-            if ((m1s<=i<m1s+nspec)or(m2s<=i<m2s+nspec))and
-            ((m1s<=j<m1s+nspec)or(m2s<=j<m2s+nspec))
-            else 0)
-    # symplectic sqs
-    ms01=Matrix(matsubs(block,al,c2,be,s2,0,0))
-    return ms01
-
-def matsubs(mat,aold,anew,bold,bnew,cold,cnew):
-    temp1=mat.subs(aold,anew)
-    temp2=temp1.subs(bold,bnew)
-    temp3=temp2.subs(cold,cnew)
-    return temp3
-
-def justdoitplease(transform, modes):
-    modetransform=transform*modes
-    pprint(relational.Eq(Matrix(modes[0:n]),Matrix(modetransform[0:n])))
-    return 
-
-def fixfloaterr(matrix):
-    imax=matrix.rows
-    jmax=matrix.cols
-    newmatrix=matrix[:,:]
-    for j in range(0,jmax):
-        for i in range(0,imax):
-            if (abs(newmatrix[i,j])<=10**-15):
-                newmatrix[i,j]=0
-    
-    return (newmatrix)
-
-def makematrixexact(matrix):
-    imax,jmax=matrix.shape
-    newmat=matrix[:,:] 
-    for j in range(0,jmax):
-        for i in range(0,imax):
-            newmat[i,j]=nsimplify(matrix[i,j], full=True)
-
-    return matrix
-
-def takagi_for_unitary(A):
-    ### takes a unitary matrix A such that A^T = A, ###
-    ###    returns unitary U such that A = U U^T    ###
-    N = A.shape[0]
-    U = np.eye(N)
-
-    while A.shape[0] > 0:   
-        n = A.shape[0]
-        # constructing a vector v such that A*ybar = y
-        x = np.random.rand(n)+1j*np.random.rand(n)
-        x /= np.sqrt(np.dot(x.conj(),x))
-        Axbar = np.tensordot(A,x.conj(),(1,0))
-        mu = np.dot(x.conj(),Axbar)
-
-        if np.abs(mu)>1-1e-8:
-            y = mu**.5*x
-        else:
-            y = Axbar + x; y /= np.sqrt(np.dot(y.conj(),y))
-
-        # constructing a unitary V with first column = y
-        V = np.random.random((n,n))+1j*np.random.random((n,n))
-        V[:,0] = y
-        V,__ = np.linalg.qr(V)
-        # update A
-        A = np.tensordot(np.tensordot(V.conj().T,A,(1,0)),V.conj(),(1,0))[1:,1:]
-        # update U
-        Vi = np.eye(N,dtype=complex)
-        Vi[N-n:,N-n:] = V
-        U = np.tensordot(U,Vi,(1,0))
-
-    return U
 
 ################################## start of program
 
@@ -151,43 +34,10 @@ phaseangle=pi/2
 bsmode1, bsmode2= 1,2
 bsangle=pi/4
 
-# phase shift
-omega=[None]*(nspectral)
-for i in range(0,nspectral):
-    omega[i]=symbols('omega:%d' % (i))
-
-omega[0]=pi/2
-
-########### Define squeezing symbols
-xi=[None]*(n**2)
-for i in range(0,n**2):
-    xi[i]=symbols('xi%d' % (i))
-
-
-#modes 0 & 1
-xi[1]=1
-xi[2]=2
-
-#modes 2 & 3
-xi[5]=1
-xi[6]=2
-
-
-#make active s block anti-diag
-xi[0]=0
-xi[3]=0
-xi[4]=0
-xi[7]=0
-
 #cosh & sinh placeholders
 c,s = symbols('c s')
 
 ########### Define optical modes
-a=[None]*n
-for i in range(0,nspace):
-    for j in range(0,nspectral):
-        a[i*(nspectral)+j]=symbols('a%d%d' % (i, j))
-
 print('Spatial modes =', nspace)
 print('Spectral modes =', nspectral)
 print('Total dim =', n)
@@ -198,42 +48,23 @@ print('Beamsplitter on modes, ', bsmode1, ',', bsmode2, 'angle= ', bsangle)
 
 
 ################## Make stuff happen!#######################
-modes=makemodes()
 
-#block form for symplectic matrix
-al = MatrixSymbol('alpha', n,n)
-be = MatrixSymbol('beta',n,n)
-
-#make block form for symplectic matrix
-block=BlockMatrix([[al,be],
-    [conjugate(be), conjugate(al)]])
-   
-#make tmsq on modes 0,1 squeezing xi
-msq1=makesq(sq1_mode1,sq1_mode2, xi)
-pprint(msq1)
-
-#make tmsq on modes 2,3
-msq2=makesq(sq2_mode1,sq2_mode2, xi)
-pprint(msq2)
-
-#make phase shifter
-mps=makeps(phasemode,phaseangle)
-pprint(mps)
-
-#make beamsplitter 
-mbs=makebs(bsmode1,bsmode2,bsangle)
-pprint(mbs)
+# makes a bs, ps, sq1, sq2
+m = Makeelements(n,nspectral, sq1_mode1, sq1_mode2,
+                    sq2_mode1, sq2_mode2, phasemode, phaseangle,
+                    bsmode1, bsmode2, bsangle)
+ 
 
 #do mode transformation
-transform=mps*msq2*msq1 
-justdoitplease(transform,modes)
+transform=m.bs*m.ps*m.sq2*m.sq1 
+m.justdoitplease(transform,m.modes)
 
 #############################################################
 ############### numerics ###################################
-print('Worked?')
+print('\n Numerics \n')
 
-pprint(msq1)
-S1=msq1[range(sq1_mode1,sq1_mode2+1),range(sq1_mode1+n,sq1_mode2+n+1)]
+pprint(m.sq1)
+S1=m.sq1[range(sq1_mode1,sq1_mode2+1),range(sq1_mode1+n,sq1_mode2+n+1)]
 
 pprint(S1)
 smat=Matrix(N(S1))
@@ -248,8 +79,7 @@ for j in range(0,jmax):
 
 #smat=Matrix(N(msq1))
 print('\n Squeeze matrix\n')
-pprint(smat)
-print(type(smat))
+#pprint(smat)
 
 ########################## svd ##########################
 print('\nnumerical Svd')
@@ -264,87 +94,33 @@ for j in range(0,2*n):
     for i in range(0,2*n):
         vtemp[i,j]=vfloat[i,j]
 
+# set numbers <10^-15 = 0 
 u=mp.chop(ufloat)
 s=mp.chop(stemp)
 v=mp.chop(vtemp)
 
-print('\n u \n')
-pprint(u)
-print('\n s \n')
-pprint(s)
-print('\n v \n')
-pprint(v)
-
+# reconstruct to compare 
 sqreconstruct=mp.chop(u*s*v)
 
 print('\nAfter svd doing u*s*v\n')
-pprint(sqreconstruct)
+#pprint(sqreconstruct)
 
 print('\nOriginal sq matrix\n')
-pprint(smat)
+#pprint(smat)
 
 diff=sqreconstruct-smat 
-
-pprint(diff)
+#pprint(mp.chop(diff))
 
 print(mp.norm(diff,p='inf'))
 
 if (mp.norm(diff,p='inf')<=10**-5):
     print('\n#############################################')
     print('#######        Close enough     #############')
-    print('#############################################')
+    print('#############################################\n')
 
+# 
+# pprint(m.makeps(0,[2],2))
+# bs
+# pprint(m.makebs(0,1,2))
 
-
-
-#pprint(relational.Eq(sqreconstruct,smat))
-
-"""
-print('Columns of U \neigenvects of sym*conj(sym)')
-pprint((smat*conjugate(smat)).eigenvects())
-
-print('Columns of V\neigenvects of conj(sym)*sym')
-pprint((conjugate(smat)*smat).eigenvects())
-
-#unitary=diag(u, eye(2))
-#pprint(unitary)
-#u1=TensorProduct(eye(2),unitary)
-
-u1=u
-v1=Matrix(vfloat)
-print('unitary')
-pprint(u1)
-
-print('unitary inverse')
-pprint(v1)
-
-print('Two mode squeezing matrix')
-pprint(msq1)
-print('U1 inv * tmsq * u1')
-pprint(u1*N(msq1)*v1)
-
-"""
-
-"""
-######################## Substitution for numerical values
-xi[0]=0
-xi[1]='blue'
-
-#update matrices and mode transformation
-
-   
-#make tmsq on modes 0,1 squeezing xi
-msq1=makesq(sq1_mode1,sq1_mode2, xi)
-#make tmsq on modes 2,3
-msq2=makesq(sq2_mode1,sq2_mode2, xi)
-#make phase shifter
-mps=makeps(phasemode,phaseangle)
-#make beamsplitter 
-mbs=makebs(bsmode1,bsmode2,bsangle)
-
-#do mode transformation
-justdoitplease(mbs*mps*msq2*msq1,modes)
-
-"""
-#end
 
