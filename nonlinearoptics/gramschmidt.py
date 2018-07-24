3a62ec3f2dd8b83dd4e8cab073f81994b5f3e732
Oliver <ot17409@bristol.ac.uk>
2018-07-24 14:37:21 +0100

    used semicolons to make it one line

--- nonlinearoptics/gramschmidt.py
+++ nonlinearoptics/gramschmidt.py
@@ -1,5 +1,1 @@
-from sympy import *
-
-l=[Matrix([1,0,0]), Matrix([1/3,1/3,1/3])]
-out=GramSchmidt(l,True)
-pprint(out)
+from sympy import *;l=[Matrix([1,0,0]), Matrix([1,1,1])];out=GramSchmidt(l,True);pprint(out)
