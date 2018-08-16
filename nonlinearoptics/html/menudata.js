96547649ee89edcf96eca8edd3013eebe2c5bc6c
Oliver <oliver.thomas404@gmail.com>
2018-08-16 08:00:02 +0100

    added wicks theorem txt file

--- nonlinearoptics/html/menudata.js
+++
@@ -1,5 +0,0 @@
-var menudata={children:[
-{text:"Main Page",url:"index.html"},
-{text:"Data Types List",url:"annotated.html",children:[
-{text:"Data Types List",url:"annotated.html"},
-{text:"Data Types",url:"classes.html"}]}]}
